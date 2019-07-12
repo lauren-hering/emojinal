b953ef6f65e8914e5d77adbc81e156df70255b18
lauren-hering <lauren.hering@comcast.net>
2019-07-12 11:07:41 -0500

    iteration 2 commit 2

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -20,7 +20,7 @@ var facts = [
     "swimming" : "it is in water",
     "rowing" : "it is on water",
     "basketball" : "you use a ball and a basket",
-    "running" : "it is on concrete"
+    "running" : "it is outside, inside, or on a treadmil"
     ]
     
     @IBAction func showMessage(sender: UIButton) {
@@ -31,6 +31,7 @@ var facts = [
         let alertController = UIAlertController(title: emojis[selectedSport!], message: emojiFact, preferredStyle: UIAlertController.Style.alert)
        
         alertController.addAction(UIAlertAction(title : "OK", style : UIAlertAction.Style.default, handler: nil))
+        alertController.addAction(UIAlertAction(title : "BYE", style : UIAlertAction.Style.default, handler: nil))
    
         present(alertController, animated: true, completion: nil)
     }
@@ -38,3 +39,14 @@ var facts = [
  
 }
 
+//Olympic sized pools have enough water to take 9,400 baths
+//Swimming became an Olympic sport in 1896
+
+//Rowing was the first intercollegiate sport contested in the United States. The first rowing race was between Harvard and Yale in 1852
+//Rowing is one of the original sports in the modern Olympic Games.
+
+//Women’s basketball was added to the Olympics in 1976
+//The positions of players in a basketball game are: Point guard, Shooting guard, Small forward, Power forward and Center
+
+//The fastest human foot speed on record is 12.4 m/s seen during a 100-meter sprint.
+//112 ligaments, 26 bones, 33 joints, nerves, blood vessels and tendons work together when we run.
