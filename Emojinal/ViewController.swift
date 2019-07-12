89733b107f3b37272703cd57be04c6fd130f9305
lauren-hering <lauren.hering@comcast.net>
2019-07-12 11:43:26 -0500

    commit iteration 3

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,25 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var customMessage = [
+        "swimming" : ["it is in water", "Olympic sized pools have enough water to take 9,400 baths", "Swimming became an Olympic sport in 1896"],
+        
+        "rowing" : ["it is on water", "Rowing was the first intercollegiate sport contested  in the United States. The first rowing race was between Harvard and Yale in 1852", "Rowing is one of the original sports in the modern Olympic Games."],
+       
+        "basketball" : ["you use a ball and a basket", "Women’s basketball was added to the  Olympics in 1976", "The positions of players in a basketball game are: Point guard, Shooting guard, Small forward, Power forward and Center"],
+        
+        "running" : ["you use your feet", "The fastest human footspeed on record is 12.4 m/s seen during a 100-meter sprint.", "112 ligaments, 26 bones, 33 joints, nerves, blood vessels and tendons work together when we run."]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     let emojis = ["🏊🏻‍♀️": "swimming", "🚣🏻‍♀️": "rowing", "⛹🏻‍♀️": "basketball", "🏃🏻‍♀️": "running"]
    
-var facts = [
-    "swimming" : "it is in water",
-    "rowing" : "it is on water",
-    "basketball" : "you use a ball and a basket",
-    "running" : "it is outside, inside, or on a treadmil"
-    ]
+//var facts = [
+  //  "swimming" : "it is in water",
+   // "rowing" : "it is on water",
+   // "basketball" : "you use a ball and a basket",
+  //  "running" : "it is outside, inside, or on a treadmil"
+  //  ]
     
     @IBAction func showMessage(sender: UIButton) {
         
+        let number = Int.random(in: 0 ... 2)
         let selectedSport = sender.titleLabel?.text
-        let emojiFact = facts[emojis[selectedSport!]!]
+        //let emojiFact = facts[emojis[selectedSport!]!]
         
-        let alertController = UIAlertController(title: emojis[selectedSport!], message: emojiFact, preferredStyle: UIAlertController.Style.alert)
+        let emojiMessage = customMessage[emojis[selectedSport!]!]?[number]
+        
+        let alertController = UIAlertController(title: emojis[selectedSport!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
        
         alertController.addAction(UIAlertAction(title : "OK", style : UIAlertAction.Style.default, handler: nil))
         alertController.addAction(UIAlertAction(title : "BYE", style : UIAlertAction.Style.default, handler: nil))
@@ -38,15 +51,3 @@ var facts = [
     
  
 }
-
-//Olympic sized pools have enough water to take 9,400 baths
-//Swimming became an Olympic sport in 1896
-
-//Rowing was the first intercollegiate sport contested in the United States. The first rowing race was between Harvard and Yale in 1852
-//Rowing is one of the original sports in the modern Olympic Games.
-
-//Women’s basketball was added to the Olympics in 1976
-//The positions of players in a basketball game are: Point guard, Shooting guard, Small forward, Power forward and Center
-
-//The fastest human foot speed on record is 12.4 m/s seen during a 100-meter sprint.
-//112 ligaments, 26 bones, 33 joints, nerves, blood vessels and tendons work together when we run.
